e5541ceaaf0fd4ebc126268fb273e1b29c71fcdf
saiNammi <118737990+saiNammi@users.noreply.github.com>
2024-11-08 17:42:13 +0530

    Update pyproj_ubi_9.3.sh

--- p/pyproj/pyproj_ubi_9.3.sh
+++ p/pyproj/pyproj_ubi_9.3.sh
@@ -74,13 +74,15 @@ else
 fi
 
 # test using import and printing version
+ls -ltr
 echo "printing current directory"
 pwd
 cd ..
 ls -ltr
 echo "printng current directory after cd .."
 pwd
-ls ../proj.4/build/lib/libproj.so.25
+ls -ltr
+ls ./proj.4/build/lib/libproj.so.25
 
 python3 -c "import pyproj; pyproj.show_versions()"
 if [ $? == 0 ]; then
