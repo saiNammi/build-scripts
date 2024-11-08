fb5b2a029d0474ecea90f7f108dbe32ae2d31c88
saiNammi <118737990+saiNammi@users.noreply.github.com>
2024-11-08 14:41:49 +0530

    Update validate_builds_currency.py

--- script/validate_builds_currency.py
+++ script/validate_builds_currency.py
@@ -86,7 +86,7 @@ def trigger_script_validation_checks(file_name,version, image_name):
         command = [
             "bash",
             "-c",
-            f"cd /home/tester/ && ./{file_name} {version} "
+            f"/home/tester/{file_name} {version} "
         ]
         
         container = client.containers.run(
